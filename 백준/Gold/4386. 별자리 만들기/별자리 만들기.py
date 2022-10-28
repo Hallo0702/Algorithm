a0a91dca3e067455342c88d81ebec870394f53ff
Hallo0702 <74289293+Hallo0702@users.noreply.github.com>
2022-10-29 02:21:57 +0900

    [Gold III] Title: 별자리 만들기, Time: 184 ms, Memory: 116464 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
@@ -0,0 +1,43 @@
+import sys
+import math
+import heapq
+input = sys.stdin.readline
+
+
+def find_boss(x):
+    global boss
+    if boss[x] != x:
+        boss[x] = find_boss(boss[x])
+    return boss[x]
+
+
+def union(x, y):
+    a = find_boss(x)
+    b = find_boss(y)
+
+    if a < b:
+        boss[a] = boss[b]
+    else:
+        boss[b] = boss[a]
+
+
+N = int(input())
+stars = [tuple(map(float, input().split())) for _ in range(N)]
+d = []
+
+for i in range(N-1):
+    for j in range(i+1,N):
+        distance = math.sqrt((stars[i][0]-stars[j][0])**2 + (stars[i][1]-stars[j][1])**2)
+        heapq.heappush(d,(distance,i,j))
+
+global boss
+answer = 0
+boss = [i for i in range(N)]
+
+while d:
+    w, x, y = heapq.heappop(d)
+    if find_boss(x) != find_boss(y):
+        union(x,y)
+        answer += w
+
+print(round(answer,2))
