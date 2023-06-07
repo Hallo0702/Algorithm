4cf18563468773c2bc33f433292eb38d75c38ca2
Hallo0702 <74289293+Hallo0702@users.noreply.github.com>
2023-06-07 10:57:21 +0900

    [Gold III] Title: 동전 분배, Time: 904 ms, Memory: 117196 KB -BaekjoonHub

---
+++ 백준/Gold/1943. 동전 분배/동전 분배.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(3):
+    N = int(input())
+    coins = {}
+    total = 0
+    for i in range(N):
+        coin, cnt = map(int, input().split())
+        total += coin * cnt
+        coins[coin] = cnt
+
+    answer = 0
+
+    if total % 2 == 1:
+        print(answer)
+        continue
+
+    total //= 2
+
+    dp = [1] + [0] * total
+
+    for coin, cnt in coins.items():
+        for now in range(total,coin-1,-1):
+            if dp[now-coin] == 1:
+                for c in range(cnt):
+                    if now + coin * c <= total:
+                        dp[now+coin*c] = 1
+
+        if dp[total] == 1:
+            break
+
+    print(dp[total])
