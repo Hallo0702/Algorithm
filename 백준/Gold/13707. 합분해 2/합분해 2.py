6842ad5d8c2b9015fd0f6c48c988bbebd2ae9b28
Hallo0702 <74289293+Hallo0702@users.noreply.github.com>
2023-09-08 11:34:58 +0900

    [Gold IV] Title: 합분해 2, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Gold/13707. 합분해 2/합분해 2.py
+++ 백준/Gold/13707. 합분해 2/합분해 2.py
@@ -1,11 +1,3 @@
+from math import comb
 N,K = map(int, input().split())
-DP = [[0 for _ in range(K+1)] for _ in range(N+1)]
-
-for i in range(1,N+1):
-    for j in range(1,K+1):
-        if i == 1:
-            DP[i][j] = j
-        else:
-            DP[i][j] = (DP[i-1][j] + DP[i][j-1]) % 1000000000
-
-print(DP[N][K])
+print(comb(N+K-1,N) % 1000000000)
