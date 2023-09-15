bdb86b95b3194082d9e569b672c6966b60c609e3
Hallo0702 <74289293+Hallo0702@users.noreply.github.com>
2023-09-15 11:01:30 +0900

    [Gold V] Title: 시간 관리하기, Time: 144 ms, Memory: 114844 KB -BaekjoonHub

---
+++ 백준/Gold/6068. 시간 관리하기/시간 관리하기.py
@@ -0,0 +1,20 @@
+import sys
+
+N = int(input())
+arr = []
+
+for _ in range(N):
+    T, S = map(int, input().split())
+    arr.append((T,S))
+
+arr.sort(key=lambda x:-x[1])
+
+answer = sys.maxsize
+for i in range(N):
+    T, S = arr[i]
+    if answer > S:
+        answer = S
+
+    answer -= T
+
+print(max(answer,-1))
