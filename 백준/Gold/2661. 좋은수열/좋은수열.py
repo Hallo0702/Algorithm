3f05516cf549c4d7219a8b40e6e99f5c0e1f553c
Hallo0702 <74289293+Hallo0702@users.noreply.github.com>
2023-10-11 10:53:37 +0900

    [Gold IV] Title: 좋은수열, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2661. 좋은수열/좋은수열.py
+++ 백준/Gold/2661. 좋은수열/좋은수열.py
@@ -13,14 +13,15 @@ def check(sequence):
 def dfs(seq): # 맨 앞자리에서 부터 시작
 
     if len(seq) == N:
-        print(seq)
-        exit()
+        return seq
 
     for number in ['1','2','3']: # 123 순서대로 확인
         if check(seq+number):
-            dfs(seq+number)
+            ans = dfs(seq+number)
+            if ans != '':
+                return ans
 
-    return
+    return ''
 
 
-dfs('')
+print(dfs(''))
