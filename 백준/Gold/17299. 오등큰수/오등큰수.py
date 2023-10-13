df6efac41aeb30be8ad9bc5f908eb73fd2568cf2
Hallo0702 <74289293+Hallo0702@users.noreply.github.com>
2023-10-13 11:08:45 +0900

    [Gold III] Title: 오등큰수, Time: 1144 ms, Memory: 156632 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.py
+++ 백준/Gold/17299. 오등큰수/오등큰수.py
@@ -1,21 +1,24 @@
 import sys
 from collections import defaultdict
 
-input = sys.stdin.readline
+def ngf():
+    input = sys.stdin.readline
 
-N = int(input())
-arr = list(map(int, input().split()))
-F = defaultdict(int)
-for i in range(N):
-    F[arr[i]] += 1
+    N = int(input())
+    arr = list(map(int, input().split()))
+    F = [0] * 1000001
+    for i in range(N):
+        F[arr[i]] += 1
 
-stack = []
-answer = [-1] * N
+    stack = []
+    answer = [-1] * N
 
-for i in range(N):
-    while stack and F[arr[stack[-1]]] < F[arr[i]]:
-        answer[stack.pop()] = arr[i]
+    for i in range(N):
+        while stack and F[arr[stack[-1]]] < F[arr[i]]:
+            answer[stack.pop()] = arr[i]
 
-    stack.append(i)
+        stack.append(i)
 
-print(*answer)
+    print(*answer)
+
+ngf()
