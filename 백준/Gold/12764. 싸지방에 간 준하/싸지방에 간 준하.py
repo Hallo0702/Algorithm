5898ab7ed762fcc7e1ef997c2f5aac9354413aae
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2023-10-18 10:56:15 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 3512 ms, Memory: 119864 KB -BaekjoonHub

---
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.py
@@ -0,0 +1,29 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+q = []
+for i in range(N):
+    start, end = map(int, input().rstrip().split())
+    heapq.heappush(q,(start,end))
+
+computer = [0] * N
+cnt = [0] * N
+
+while q:
+    start, end = heapq.heappop(q)
+
+    for i in range(N):
+        if computer[i] <= start:
+            cnt[i] += 1
+            computer[i] = end
+            break
+
+if computer[-1] == 0:
+    X = computer.index(0)
+else:
+    X = N
+print(X)
+for i in range(X):
+    print(cnt[i],end=' ')
