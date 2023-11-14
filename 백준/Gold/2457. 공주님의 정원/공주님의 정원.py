d7cee95ec440b57cb025ae6672c44081fcf190e7
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2023-11-14 11:28:55 +0900

    [Gold III] Title: 공주님의 정원, Time: 2044 ms, Memory: 46628 KB -BaekjoonHub

--- 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
+++ 백준/Gold/2457. 공주님의 정원/공주님의 정원.py
@@ -1,53 +1,40 @@
 import sys
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-date = []
+N = int(input())
 
-# 편의를 위해 100을 곱해 날짜 형식으로 바꿈
-for _ in range(n):
-    temp = list(map(int, sys.stdin.readline().split()))
-    date.append([temp[0] * 100 + temp[1], temp[2] * 100 + temp[3]])
+flowers = []
 
-# 꽃이 피고 지는 날짜를 오름차순으로 정렬
-date.sort(key=lambda x:(x[0], x[1]))
-# 선택한 꽃의 개수
-cnt = 0
-# 제일 늦게 지는 꽃을 비교
-end = 0
-# 마지막 꽃의 지는 날
-target = 301
+for i in range(N):
+    b_m,b_d, w_m,w_d = map(int, input().split())
+    flowers.append([100*b_m + b_d , 100*w_m+w_d])
 
-# 모든 꽃이 없어질 때까지 반복하여 꽃을 비교한다.
-while date:
-    # 마지막 꽃의 지는날이 12월 1일 보다 크거나 같을 때와
-    # 마지막 꽃의 지는날이 제일 빨리 피는 꽃보다 작으면 멈춘다.
-    if target >= 1201 or target < date[0][0]:
+flowers.sort()
+
+last_day = 301
+last = 0
+answer = 0
+
+while flowers:
+
+    if last_day >= 1201 or last_day < flowers[0][0]:
         break
 
-    # 꽃의 개수의 길이만큼 반복하여 구간별로 꽃을 비교한다.
-    for _ in range(len(date)):
-        # 마지막 꽃의 지는 날이 제일 빨리 피는 꽃보다 크거나 같으면 그 꽃의 지는 날을 확인한다.
-        if target >= date[0][0]:
-            # 그 꽃의 지는 날과 마지막으로 꽃의 지는 날을 비교한다.
-            # 그 꽃의 지는 날이 더 크면 더 오래 꽃을 볼 수 있기때문에
-            # 그 꽃의 지는 날을 마지막 꽃의 지는 날로 바꾼다.
-            if end <= date[0][1]:
-                end = date[0][1]
+    for i in range(len(flowers)):
+        if last_day >= flowers[0][0]:
+            if last <= flowers[0][1]:
+                last = flowers[0][1]
 
-            # 꽃을 확인 하면 제거한다.
-            date.remove(date[0])
+            flowers.remove(flowers[0])
 
-        # 꽃의 지는 날이 제일 빨리 피는 꽃보다 작으면 멈춰준다.
         else:
             break
 
-    # 최종적으로 선택한 꽃의 지는 날을 바꾼다.
-    target = end
-    # 꽃을 선택했으므로 카운트한다.
-    cnt += 1
+    last_day = last
+    answer += 1
 
-# 마지막 꽃의 지는 날이 12월 1일보다 작으면 11월 30일에는 피어있는 꽃이 없기때문에 0을 출력
-if target < 1201:
+
+if last_day < 1201:
     print(0)
 else:
-    print(cnt)
+    print(answer)
