96b548faa4f0b0f79ae6d18e82d32abe847ac215
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2023-12-15 11:40:22 +0900

    [Gold III] Title: 게임 닉네임, Time: 384 ms, Memory: 67892 KB -BaekjoonHub

--- 백준/Gold/16934. 게임 닉네임/게임 닉네임.py
+++ 백준/Gold/16934. 게임 닉네임/게임 닉네임.py
@@ -6,44 +6,22 @@ users = []
 for i in range(N):
     users.append(input().rstrip())
 
-
-class Node(object):
-    def __init__(self,key,data=None):
-        self.key = key
-        self.data = data
-        self.count = 0
-        self.children = {}
-
-
-class Trie(object):
-    def __init__(self):
-        self.head = Node(None)
-
-    def search_prefix(self,string):
-        now = self.head
-        result = ''
-        for s in string:
-            result += s
-            if s in now.children:
-                now = now.children[s]
-            else:
-                return result
-
-        if now.data != None:
-            result += str(now.count + 1)
-        return result
-
-    def insert_node(self,string):
-        now = self.head
-        for s in string:
-            if s not in now.children:
-                now.children[s] = Node(s)
-            now = now.children[s]
-        now.data = string
-        now.count += 1
-
-
-trie = Trie()
+count = dict()
+pre = set()
 for user in users:
-    print(trie.search_prefix(user))
-    trie.insert_node(user)
+    if user in count:
+        count[user] += 1
+        print(user + str(count[user]))
+    else:
+        count[user] = 1
+        result = ''
+        flag = 0
+        for c in user:
+            result += c
+            if result not in pre and flag == 0:
+                print(result)
+                flag = 1
+            pre.add(result)
+
+        if flag == 0:
+            print(result)
