3f6feae6d5c0b319fc5b1e4beaaf5a2c53604c58
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-01-25 11:13:53 +0900

    [Gold III] Title: ㅋㅋ루ㅋㅋ, Time: 1760 ms, Memory: 38852 KB -BaekjoonHub

--- 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.py
+++ 백준/Gold/20442. ㅋㅋ루ㅋㅋ/ㅋㅋ루ㅋㅋ.py
@@ -7,29 +7,29 @@ for i in range(len(KKRKK)):
 
 left_k, right_k = 0, 0
 left, right = 0, len(KKRKK)-1
-answer = 0
-while left <= right:
+answer = cnt
 
-    if KKRKK[left] == 'R' and KKRKK[right] == 'R':
-        answer = max(answer, cnt+2*min(left_k,right_k))
-        if left_k > right_k:
-            right -= 1
-            cnt -= 1
-        elif left_k < right_k:
-            left += 1
-            cnt -= 1
-        else:
-            left += 1
-            right -= 1
-            cnt -= 2
-        continue
+while cnt > 0:
 
-    if KKRKK[left] == 'K':
+    while cnt and KKRKK[left] == 'K':
         left += 1
         left_k += 1
 
-    if KKRKK[right] == 'K':
+    while cnt and KKRKK[right] == 'K':
         right -= 1
         right_k += 1
+
+    answer = max(answer, cnt + 2*min(left_k,right_k))
+
+    if left_k > right_k:
+        right -= 1
+        cnt -= 1
+    elif left_k < right_k:
+        left += 1
+        cnt -= 1
+    else:
+        left += 1
+        right -= 1
+        cnt -= 2
 
 print(answer)
