e969d6f8a2e8c726567637b47cadba8695a36ede
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-09-24 16:28:05 +0900

    [Gold IV] Title: 로또, Time: 36 ms, Memory: 32140 KB -BaekjoonHub

--- 백준/Gold/2758. 로또/로또.py
+++ 백준/Gold/2758. 로또/로또.py
@@ -1,29 +1,6 @@
-# 2번 최대 범위 먼저 구하고 풀기
 import sys
 input = sys.stdin.readline
 
-from collections import deque
-T = int(input())
-nm = deque()
-max_n, max_m = 0, 0
-for t in range(T):
-    n, m = map(int, input().split())
-    if n > max_n:
-        max_n = n
-    if m > max_m:
-        max_m = m
-    nm.append((n,m))
-
-DP = [[1]*(max_m+1)] + [[0]*(max_m+1) for _ in range(max_n+1)]
-
-for i in range(1,max_n+1):
-    for j in range(i,max_m+1):
-        DP[i][j] = DP[i][j-1] + DP[i-1][j//2]
-
-for N, M in nm:
-    print(DP[N][M])
-'''
-# 1번 전체 구해놓고 하나씩 하기
 T = int(input())
 DP = [[1]*2001] + [[0]*2001 for _ in range(10)]
 
@@ -34,4 +11,3 @@ for i in range(1,11):
 for t in range(T):
     n, m = map(int, input().split())
     print(DP[n][m])
-'''
