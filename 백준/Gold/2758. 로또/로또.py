2466fbb1420e461f0e94327ddcc664243f169edd
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-09-24 16:19:49 +0900

    [Gold IV] Title: 로또, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2758. 로또/로또.py
@@ -0,0 +1,10 @@
+T = int(input())
+DP = [[1]*2001] + [[0]*2001 for _ in range(10)]
+
+for i in range(1,11):
+    for j in range(i,2001):
+        DP[i][j] = DP[i][j-1] + DP[i-1][j//2]
+
+for t in range(T):
+    n, m = map(int, input().split())
+    print(DP[n][m])
