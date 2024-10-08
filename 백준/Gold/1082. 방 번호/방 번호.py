e8b2134d588e6b3be54e038a3c4aa4d2e55dcf78
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-10-08 16:53:39 +0900

    [Gold III] Title: 방 번호, Time: 60 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Gold/1082. 방 번호/방 번호.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+N = int(input())
+P = list(map(int, input().split()))
+M = int(input())
+
+DP = [0] * (M+1)
+q = deque()
+
+for i in range(1,N):
+    if P[i] <= M:
+        q.append((i,P[i]))
+        DP[P[i]] = i
+
+while q:
+    number, price = q.popleft()
+    if DP[price] > number:
+        continue
+
+    for j in range(N):
+        next_num = int(str(number)+str(j))
+        if price + P[j] <= M and DP[price + P[j]] < next_num:
+            DP[price+P[j]] = next_num
+            q.append((next_num,price+P[j]))
+
+print(max(DP))
