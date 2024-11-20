0e6a3154029bc2fd328e137b39f2c245134d4124
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-11-20 16:48:58 +0900

    [Gold III] Title: 백양로 브레이크, Time: 512 ms, Memory: 112056 KB -BaekjoonHub

---
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+road = [[sys.maxsize for _ in range(N+1)] for _ in range(N+1)]
+for i in range(1,N+1): # 같은 길은 우선 0 으로
+    road[i][i] = 0
+
+for m in range(M): # 한 방향 -> 0 / 1 양방향 0/0
+    u, v, b = map(int, input().split())
+    if b == 0:
+        road[u][v] = 0
+        road[v][u] = 1
+    else:
+        road[u][v] = 0
+        road[v][u] = 0
+for k in range(1,N+1):
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            road[i][j] = min(road[i][j],road[i][k]+road[k][j])
+
+k = int(input())
+for _ in range(k):
+    a, b = map(int, input().split())
+    print(road[a][b])
