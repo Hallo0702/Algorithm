b550d23fb3c1431e6803acf37cbeb4adff946f5d
홍찬기 <74289293+Hallo0702@users.noreply.github.com>
2024-12-04 16:34:27 +0900

    [Gold IV] Title: Puyo Puyo, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -1,58 +1,52 @@
-from collections import deque
+def break_puyo():
+    global flag
+    visit = [[0]*6 for _ in range(12)]
 
-dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    for i in range(12):
+        for j in range(6):
+            if not visit[i][j] and puyo[i][j] != '.':
+                visit[i][j] = 1
+                q = [(i,j)]
+                waiting = [(i,j)]
 
-def gravity(): # 밑으로 떨어트리기
-    for x in range(6):
-        q = deque()
-        for y in range(11, -1, -1):
-            if board[y][x] != '.':
-                q.append(board[y][x]) # 아래에서부터 차례로 색상 담기
-        for y in range(11, -1, -1):
-            if q:
-                board[y][x] = q.popleft() # 차례로 색상 꺼내기
-            else:
-                board[y][x] = "."
+                while q:
+                    x, y = q.pop()
+                    for d in [(0,1),(1,0),(-1,0),(0,-1)]:
+                        nx, ny = x + d[0], y + d[1]
+                        if 0 <= nx < 12 and 0 <= ny < 6 and not visit[nx][ny] and puyo[nx][ny] == puyo[x][y]:
+                            visit[nx][ny] = 1
+                            q.append((nx,ny))
+                            waiting.append((nx,ny))
 
-def BFS(y,x): # 연결되어 있는 같은 색상 => 터트리기
-    check = [[0]*6 for _ in range(12)]
-    q = deque()
-    boom = deque()
-    q.append((y, x))
-    boom.append((y, x))
-    target = board[y][x]
-    check[y][x] = 1
-    while q:
-        ny, nx = q.popleft()
-        for i, j in dir:
-            dy, dx = ny+i, nx+j
-            if 0 <= dy < 12 and 0 <= dx < 6:
-                if board[dy][dx] == target and check[dy][dx] == 0:
-                    check[dy][dx] = 1
-                    q.append((dy, dx))
-                    boom.append((dy, dx))
-    if len(boom) >= 4: # 연결된 게 4개 이상이면
-        for y, x in boom:
-            board[y][x] = '.' #터트리기
-        return 1
-    return 0
+                if len(waiting) >= 4:
+                    flag = 1
+                    for i, j in waiting:
+                        puyo[i][j] = '.'
 
-color = []
-board = []
 
-for _ in range(12):
-    board.append(list(input()))
-ans = 0
+def down_puyo():
+    for j in range(6):
+        for i in range(11,-1,-1):
+            if puyo[i][j] == '.':
+                bot = i
+                break
+        else:
+            continue
 
-while True:
-    turn = 0
-    for y in range(12):
-        for x in range(6):
-            if board[y][x] != '.':
-                turn += BFS(y, x)
-    if turn == 0:
-        print(ans)
-        break
-    else:
-        ans += 1
-    gravity() # 턴 한 번 돌고 밑으로 떨어트리기
+        for i in range(bot-1,-1,-1):
+            if puyo[i][j] != '.':
+                puyo[bot][j], puyo[i][j] = puyo[i][j], puyo[bot][j]
+                bot -= 1
+
+
+
+puyo = [list(input()) for _ in range(12)]
+answer = 0
+flag = 1
+while flag:
+    flag = 0
+    break_puyo()
+    if flag:
+        answer += 1
+        down_puyo()
+print(answer)
